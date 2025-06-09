fce9b3430afdc670fa433154d90f81bc1096f9ed
krutoifuflik <221653@astanait.edu.kz>
2025-06-10 00:32:19 +0500

    fix7 viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['frontfordeploy-production.up.railway.app']
+  },
   plugins: [react(),
+  
     
   ],
 })
